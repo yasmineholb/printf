ae1c0d3b91b3e3db19a3853728d2b2085e9514c7
yasmineholb <1055@holbertonschool.com>
2019-10-29 16:14:31 +0100

    commit

--- holberton.h
+++ holberton.h
@@ -14,4 +14,8 @@ char *c;
 int (*fn)();
 } type1;
 int _printf(const char *format, ...);
+int print_c(va_list ap);
+int print_s(va_list ap);
+int print_per(va_list ap);
+int _putchar(char c);
 #endif
